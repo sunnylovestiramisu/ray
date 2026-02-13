d7321994478e532e81a337945df989346b4b578c
Sunny Song <songsunny@google.com>
2026-02-13 17:50:19 +0000

    Set commit sha from wheels and modify Dockerfile for permission

--- python/ray/_version.py
+++ python/ray/_version.py
@@ -1,5 +1,5 @@
 # Replaced with the current commit when building the wheels.
-commit = "{{RAY_COMMIT_SHA}}"
+commit = "73bf5b454a326f780594a40e0950174b18c79a89"
 version = "3.0.0.dev0"
 
 if __name__ == "__main__":
